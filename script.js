fe369f71ae49ef70ca479c138d444cdefa8469e1
Chika Sylvester <sylverkenz@gmail.com>
2022-09-22 07:17:54 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (entry.isIntersecting) entry.target.classList.remove("section--hidden");
+
+  if (entry.isIntersecting) observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.1,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  // section.classList.add('section--hidden');
+});
